845729264d2eabff8a5da68aa2ba11c3a5fee167
Emmanuel <ejawanyama@gmail.com>
2025-05-24 22:20:59 +0300

    initial interest commit

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate interest
+interest = principal * rate * time
+
+# Print the results
+print(f"Product of {principal} and {rate} and {time} is {interest}")
